3c870d749e7b5048c3eebdb529b829f5c3367065
synthresin <synthresin@me.com>
2012-09-29 12:03:19 +0900

    프로젝트 시작

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberRest::Application.config.secret_token = '898c17cb4ab087191ecfe7de4e600d13e568cc9cce39e25feb6d3a51e839d9807a4443b2e686fb4f21e890662fe94a6216d6be24d39a6d3d237809977e766c61'
